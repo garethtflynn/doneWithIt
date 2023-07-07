10d208d211ffd21ff49fb8adfe452fc9d761e5d1
Gareth Flynn <gareth.t.flynn@gmail.com>
2023-07-07 10:27:19 -0400

    cardComponent & listingDetails

--- app/config/colors.js
+++ app/config/colors.js
@@ -2,5 +2,7 @@ export default {
     primary: '#fc5c65',
     secondary: '#4ecdc4',
     black: '#000',
-    white: '#fff'
+    white: '#fff',
+    cardBackground: '#f8f4f4',
+    medium: '#6e6969'
 }
