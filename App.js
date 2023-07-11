730f7e5126879e1c21693a82dd2bcad849252066
Gareth Flynn <gareth.t.flynn@gmail.com>
2023-07-11 13:50:51 -0400

    account & listings screen added

--- App.js
+++ App.js
@@ -5,9 +5,14 @@ import { View } from "react-native";
 import AppCard from "./app/components/AppCard";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import AccountScreen from "./app/screens/AccountScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
   return (
-      <MessagesScreen />
+    <ListingsScreen/>
   );
 }
