fbe2964ee4bf659f1cf44e5ac2615cc9b05750c1
Gareth Flynn <gareth.t.flynn@gmail.com>
2023-07-31 17:16:43 -0400

    added all navigation functionality

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Button, Image } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ImageScreen from "./app/screens/ImageScreen";
-import AppCard from "./app/components/AppCard";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/lists/ListItem";
-import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import * as Permissions from "expo-permissions";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import FeedNavigator from "./app/navigation/FeedNavigator";
+import AccountNavigator from "./app/navigation/AccountNavigator";
+
+
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
